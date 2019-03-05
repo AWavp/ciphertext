328de645f0c0031c2d8f075f9d5c44531c8b7b58
AWavp <47437282+AWavp@users.noreply.github.com>
2019-03-05 09:50:34 -0700

    Add files via upload

---
+++ ciphertext/Go here/CA/cipher12.py
@@ -0,0 +1,20 @@
+w = str('''To start your quest you will have the numbers 17, 35,and 22. Now you will you have to coose a mod or modulus, if you don't what it is then learn it,
+you can choose between 2 or 4 for mod''')
+
+s = int(input("Enter your choosing number: "))
+k = int(input("Enter your mod: "))
+w = int
+w = s %k
+
+if s == 22 and k == 4 and w == 2:
+    print(''' Hint: there is a certain combination that must be used to get phase one
+don't forget multiplcation, divison, addition, and subtraction. P.S. this is your only hint. ''')
+
+elif w != 2 and 4:
+    print(''' Try again nerd''')
+
+elif s == 30 and k == 4 and w == 2:
+    print("560, 011, 611, 401, 111, 110, 112, 780, 501, 801, 501, 709, 901, 511,and there is a shift between 1-3 either right or left")
+
+print (w)    
+    
---
+++ ciphertext/Go here/CA/readme.py
@@ -0,0 +1,5 @@
+p = str('''To start your quest you will have the numbers 17, 35,and 22. Now you will you have to coose a mod or modulus, if you don't what it is then learn it,
+you can choose between 2 or 4 for mod and then cipher12 and cipher123 in order''')
+
+print(p)
+
