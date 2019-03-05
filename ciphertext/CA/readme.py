e626c2e7ace25a4dffde716991fb4a952c0836dc
AWavp <47437282+AWavp@users.noreply.github.com>
2019-03-05 09:43:49 -0700

    Update readme.py

--- ciphertext/CA/readme.py
+++ ciphertext/CA/readme.py
@@ -1,5 +1,1 @@
-p = str('''To start your quest you will have the numbers 17, 35,and 22. Now you will you have to coose a mod or modulus, if you don't what it is then learn it,
-you can choose between 2 or 4 for mod and then run the second batch file''')
-
-print(p)
-
+you will have to use pip install pyinstaller in powershell and then you will have to put the files in your C:\Users to run
